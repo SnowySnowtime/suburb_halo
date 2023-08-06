dfca6f840b145db00ff87cd9e711c1aa5c1296d7
Snowy Snowtime <47854261+SnowySnowtime@users.noreply.github.com>
2023-08-06 05:24:49 -0700

    Initial commit

---
+++ lua/autorun/uc_snow_halo_snds.lua
@@ -0,0 +1,18 @@
+-- Battle Rifle
+
+sound.Add(
+{
+    name = "uchalo.battlerifle.fire",
+    channel = CHAN_STATIC,
+    volume = 1.0,
+    soundlevel = 115,
+    sound = {"snowysnowtime/h3/battlerifle/fire_burst_h3/battle_rifle_fire1.wav",
+	"snowysnowtime/h3/battlerifle/fire_burst_h3/battle_rifle_fire2.wav",
+	"snowysnowtime/h3/battlerifle/fire_burst_h3/battle_rifle_fire3.wav",
+	"snowysnowtime/h3/battlerifle/fire_burst_h3/battle_rifle_fire4.wav",
+	"snowysnowtime/h3/battlerifle/fire_burst_h3/battle_rifle_fire5.wav",
+	"snowysnowtime/h3/battlerifle/fire_burst_h3/battle_rifle_fire6.wav",
+	"snowysnowtime/h3/battlerifle/fire_burst_h3/battle_rifle_fire7.wav",
+	"snowysnowtime/h3/battlerifle/fire_burst_h3/battle_rifle_fire8.wav",
+	}
+})
